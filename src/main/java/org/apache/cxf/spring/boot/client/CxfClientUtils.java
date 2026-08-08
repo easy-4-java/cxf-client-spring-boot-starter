550df9db37c23c2e7001971e00bb09f10213c5fe
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:27:29 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.0.x branch

--- src/main/java/org/apache/cxf/spring/boot/client/CxfClientUtils.java
+++ src/main/java/org/apache/cxf/spring/boot/client/CxfClientUtils.java
@@ -25,7 +25,7 @@ public class CxfClientUtils {
 	/**
 	 * 
 	 * @description	： 动态调用webService接口
-	 * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @date 		：2017年11月13日 下午5:20:28
 	 * @param wsdlUrl 地址(带 ?wsdl)
 	 * @param optName 调用的操作名称(一般为方法名称)
